9d05a3112c2b3bc91b7448b7e909dbb0110e8f97
Jeongsu Yang <mycat83@gmail.com>
2020-08-06 18:24:56 +0900

    Initial commit

---
+++ socket-client/src/main/java/me/study/socket/client/SocketClientApplication.java
@@ -0,0 +1,13 @@
+package me.study.socket.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocketClientApplication.class, args);
+    }
+
+}
