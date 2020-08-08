4141ace2fc2116ae7d1d65374575d57759bb302d
Jeongsu Yang <mycat83@gmail.com>
2020-08-08 22:05:47 +0900

    리팩토링

--- socket-server/src/main/java/me/study/socket/server/SocketRunner.java
+++ socket-server/src/main/java/me/study/socket/server/SocketRunner.java
@@ -1,5 +1,6 @@
 package me.study.socket.server;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -12,38 +13,41 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Date;
+
+/*
+[공통부분]
+전문길이(4), 업무구분(4), 일자(8), 시간(6)
+
+[DATA부분]
+신청(0001)
+이름(10), 신청금액(10)
+0042 A001 20200807 093000 'YANG      ' 0100000000
+
+신청응답(1001)
+승인여부(1), 승인금액(10)
+0033 A001 20200807 093000 1 0090000000
+
+0042A00120200807093000YANG      0100000000
+0033B0012020080709300010090000000
+ */
 
 @Component
 public class SocketRunner implements ApplicationRunner {
 
+    @Value("${app.socket-server.port}")
+    private int port;
+
+    @Value("${app.socket-server.enable}")
+    private boolean enable;
+
     @Override
     public void run(ApplicationArguments args) {
-
-        /*
-        [공통부분]
-        전문길이(4), 업무구분(4), 일자(8), 시간(6)
-
-        [DATA부분]
-        신청(0001)
-        이름(10), 신청금액(10)
-        0042 A001 20200807 093000 'YANG      ' 0100000000
-
-        신청응답(1001)
-        승인여부(1), 승인금액(10)
-        0033 A001 20200807 093000 1 0090000000
-
-        0042A00120200807093000YANG      0100000000
-        0033B0012020080709300010090000000
-         */
-
-       int port = 8000;
 
         try (ServerSocket serverSocket = new ServerSocket(port)) {
 
             System.out.println("Server is listening on port " + port);
 
-            while (true) {
+            while (enable) {
                 Socket socket = serverSocket.accept();
                 System.out.println("[ " + socket.getInetAddress() + " ] client connected");
 
@@ -54,7 +58,6 @@ public class SocketRunner implements ApplicationRunner {
                 PrintWriter writer = new PrintWriter(output, true);
 
                 String line;
-
                 while ((line = reader.readLine()) != null) {
                     System.out.println("Message received: " + line);
 
